1de53b4a390cbc7b1cbe488ae8c9c68def3daac1
John <132560971+Krokuta001@users.noreply.github.com>
2023-09-21 00:27:07 +0300

    Final

--- errors.c
+++ errors.c
@@ -8,78 +8,73 @@
  */
 void _eputs(char *str)
 {
-	int i = 0;
+	int s = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
+	while (str[s] != '\0')
 	{
-		_eputchar(str[i]);
-		i++;
+		_eputchar(str[s]);
+		s++;
 	}
 }
 
 /**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _eputchar(char c)
+writeCharToStderr - writes the character 'c' to stderr
+@c: The character to be printed
+Return: 1 on success. On error, -1 is returned, and errno is set appropriately.
+*/
+int _eputchar(char z)
 {
-	static int i;
+	static int s;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (z == BUF_FLUSH || s >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, i);
-		i = 0;
+		write(2, buf, s);
+		s = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (z != BUF_FLUSH)
+		buf[s++] = z;
 	return (1);
 }
 
 /**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putfd(char c, int fd)
+writeCharToFileDescriptor - writes the character 'c' to the specified file descriptor 'fd'
+@c: The character to print
+@fd: The file descriptor to write to
+Return: 1 on success. On error, -1 is returned, and errno is set appropriately.
+*/
+int _putfd(char z, int d)
 {
-	static int i;
+	static int s;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (z == BUF_FLUSH || s >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(d, buf, s);
+		s = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (z != BUF_FLUSH)
+		buf[s++] = z;
 	return (1);
 }
 
 /**
- * _putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
- */
-int _putsfd(char *str, int fd)
+printInputStringToFileDescriptor - prints a given input string to the specified file descriptor
+@str: the string to be printed
+@fd: the file descriptor to write to
+Returns: the number of characters written
+*/
+int _putsfd(char *str, int d)
 {
-	int i = 0;
+	int s = 0;
 
 	if (!str)
 		return (0);
 	while (*str)
 	{
-		i += _putfd(*str++, fd);
+		s += _putfd(*str++, d);
 	}
-	return (i);
+	return (s);
 }
