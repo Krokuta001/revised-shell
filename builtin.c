7c6ca00cca6ff9ee967d97b92d6c1208180b3b81
John <132560971+Krokuta001@users.noreply.github.com>
2023-09-20 22:30:38 +0300

    Final

--- builtin.c
+++ builtin.c
@@ -1,96 +1,47 @@
 #include "shell.h"
 
-/**
- * _myexit - exits the shell
- * @info: the Structure containing arguments. Used to maintain function prototype
- * constant function prototype.
- * Return: with a given exit status return exit
- * (0) if info.argv[0] != "exit"
- */
-int _myexit(info_t *info)
+#define MAX_INPUT_LENGTH 1024
+
+int main()
 {
-	int exitcheck;
+    char input[MAX_INPUT_LENGTH];
 
-	if (info->argv[1]) /* If there is an exit arguement */
-	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
-		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
-			return (1);
-		}
-		info->err_num = _erratoi(info->argv[1]);
-		return (-2);
-	}
-	info->err_num = -1;
-	return (-2);
-}
+    while (1)
+    {
+        printf("simple_shell$ "); // Display a prompt
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            // Handle end of file (Ctrl+D)
+            printf("\n");
+            break;
+        }
 
-/**
- * _mycd -  this changes the current directory
- * @info: the struct containing potential arguments.for prototype
- * Return: Always 0
- */
-int _mycd(info_t *info)
-{
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
+        // Remove the newline character
+        input[strlen(input) - 1] = '\0';
 
-	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-		else
-			chdir_ret = chdir(dir);
-	}
-	else if (_strcmp(info->argv[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(s);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
-	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-	return (0);
-}
+        // Fork a child process
+        pid_t pid = fork();
 
-/**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
- */
-int _myhelp(info_t *info)
-{
-	char **arg_array;
+        if (pid == -1)
+        {
+            perror("Fork failed");
+            exit(1);
+        }
+        else if (pid == 0)
+        {
+            // Child process
+            execlp(input, input, (char *)NULL);
 
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
-	return (0);
+            // If execlp fails, it means the command is not found
+            perror("Command not found");
+            exit(1);
+        }
+        else
+        {
+            // Parent process
+            wait(NULL); // Wait for the child process to finish
+        }
+    }
+
+    return 0;
 }
