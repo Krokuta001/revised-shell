baedf25f9bee8186ca09383a12c2e2d438046d5d
John <132560971+Krokuta001@users.noreply.github.com>
2023-09-21 02:27:22 +0300

    Final

--- builtin.c
+++ builtin.c
@@ -44,7 +44,7 @@ int _mycd(info_t *info)
 	{
 		dir = _getenv(info, "HOME=");
 		if (!dir)
-			chdir_ret = 
+			chdir_ret = " "
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
 		else
 			chdir_ret = chdir(dir);
@@ -58,7 +58,7 @@ int _mycd(info_t *info)
 			return (1);
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = 
+		chdir_ret = " "
 			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
