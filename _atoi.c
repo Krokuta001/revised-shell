07be913eb754c89e833ed1937b577f318823f3b6
John <132560971+Krokuta001@users.noreply.github.com>
2023-09-20 22:12:13 +0300

    Final

--- _atoi.c
+++ _atoi.c
@@ -1,74 +1,28 @@
-#include "shell.h"
+#include <stdio.h>
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
+ * print_hello - This function prints "Hello, Betty!" to the standard output.
  */
-int interactive(info_t *info)
+void print_hello(void)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+    printf("Hello, Betty!\n");
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ * main - The entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ * Return: 0 on success.
  */
-int is_delim(char c, char *delim)
+int main(int argc, char *argv[])
 {
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
-}
+    if (argc != 1)
+    {
+        printf("Usage: %s\n", argv[0]);
+        return 1;
+    }
 
-/**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
- */
+    print_hello();
 
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
- */
-
-int _atoi(char *s)
-{
-	int i, sign = 1, flag = 0, output;
-	unsigned int result = 0;
-
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
+    return 0;
 }
